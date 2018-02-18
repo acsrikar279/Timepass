d3effec9fce345c4dfda3a37dcf3728e966a475d
acsrikar279 <acsrikar@gmail.com>
2018-02-18 09:15:59 +0530

    Update first.py
    Encoded results to ASCII

--- first.py
+++ first.py
@@ -4,6 +4,7 @@ names,gpa = [],[]
 print('Enter the roll numbers seperated by spaces')
 a=raw_input()
 rolls=a.split(' ')
+index=0
 for i in rolls:
         br = mechanize.Browser()
         br.open('http://gvpce.ac.in/results/B.Tech%20V%20Sem%20Regular%20(R-2015)%20(For%202015%20batch)%20Result_October-%202017/btechsearch.asp')
@@ -14,21 +15,17 @@ for i in rolls:
         names.append(soup("td",{'colspan':'4'})[1].text)
         gpa.append(soup("td",{'colspan':'4'})[2].text)
         br.close()
-print(names)
-print(gpa)
-'''
-import matplotlib.pyplot as plt; plt.rcdefaults()
+        names[index] = names[index].encode('ascii')
+        gpa[index] =gpa[index].encode('ascii')
+        index+=1
+index=0
+
 import numpy as np
 import matplotlib.pyplot as plt
- 
-objects = tuple(rolls)
-y_pos = np.arange(len(objects))
-performance = marks
- 
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Usage')
-plt.title('Programming language usage')
- 
+
+x, y = np.array(range(0,len(names))), gpa
+axes = plt.gca()
+axes.set_ylim([0,10])
+plt.xticks(x,names)
+plt.bar(x, y, width=0.5)
 plt.show()
-'''
